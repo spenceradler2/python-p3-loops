8a0af1c111eee903e33b7849c73bca27383b52a1
Spencer <spencer.adler2@gmail.com>
2024-06-25 17:20:22 -0400

    done

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,25 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    i=10
+    while i>0:
+        print(f"{i}")
+        i-=1
+    print("Happy New Year!")
+    
 
 def square_integers(int_list):
-    # code goes here!
-    pass
-
+    return [i**2 for i in int_list]
+        
 def fizzbuzz():
-    # code goes here!
-    pass
+    i=1
+    while i<=100:
+        if i%3 == 0 and i%5 == 0:
+            print("FizzBuzz")
+        elif i%3 == 0:
+            print("Fizz")
+        elif i%5 == 0:
+            print("Buzz")
+        else:
+            print(f"{i}")
+        i+=1
